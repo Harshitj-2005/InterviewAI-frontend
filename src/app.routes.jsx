b898130e17f53b12103dcc59c9424a92d8cf3546
Jain <jainharshit59954@gmail.com>
2026-03-19 02:24:26 +0530

    error

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Register from "./features/auth/pages/Register";
 import Login from "./features/auth/pages/Login";
 import Protected from "./features/auth/components/protected";
-import Home from "./features/interview/pages/Home";
+import Home from "./features/interview/pages/HomeComponent";
 import Interview from "./features/interview/pages/interview";
 
 export const router = createBrowserRouter([

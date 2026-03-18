bc5f3cabbf36ecbbb12b14aa5e59490015815ad5
Jain <jainharshit59954@gmail.com>
2026-03-19 02:07:11 +0530

    wq :wq

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router";
 import Register from "./features/auth/pages/Register";
 import Login from "./features/auth/pages/Login";
 import Protected from "./features/auth/components/protected";

8d8f16df861df9fdebba2f95f6b659b9c53ed80f
Harshit Jain <jainharshit59954@gmail.com>
2026-05-31 21:18:57 +0530

    Update baseURL assignment to use nullish coalescing

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL ?? '',
     withCredentials: true
 })
+
 
 //use to generate interview report
 export const generateInterviewReport = async ({ resume, jobDescription, selfDescription }) => {
